de844b68203b30e6c8716f680e872eef15623a93
Yilmaz Dogus <yilmaz@evorine.com>
2017-12-04 23:07:12 +0100

    Added unit test for layout calculation

--- tests/NextPlatform.Layout.Tests/BasicTestData.cs
+++ tests/NextPlatform.Layout.Tests/BasicTestData.cs
@@ -12,20 +12,20 @@ namespace NextPlatform.Layout.Tests
     {
         /* Layout is:
          * <component Width="Fill" Height="Fill" LayoutDirection="Vertical">
-         *   <component Width="Fill" Height="20u" />
+         *   <component Width="Fill" Height="50px" />
          *   <component Width="Fill" Height="Fill" LayoutDirection="Horizontal">
          *     <component Width="1x" Height="Shrink" LayoutDirection="Vertical">
-         *       <component Width="Fill" Height="10u" />
-         *       <component Width="Fill" Height="20u" />
+         *       <component Width="Fill" Height="40px" />
+         *       <component Width="Fill" Height="40px" />
          *     </component>
          *     <component Width="3x" Height="Fill" LayoutDirection="Vertical">
-         *       <component Width="Fill" Height="35u" />
-         *       <component Width="Fill" Height="20u" />
+         *       <component Width="Fill" Height="40px" />
+         *       <component Width="Fill" Height="80px" />
          *     </component>
          *   </component>
          * </component>
          * */
-
+        
         public VisualElement ComponentRoot { get; }
         public VisualElement ComponentTop { get; }
         public VisualElement ComponentBottom { get; }
@@ -42,14 +42,14 @@ namespace NextPlatform.Layout.Tests
             ComponentRoot.Name = "Root";
             ComponentRoot.Width = Length.Fill;
             ComponentRoot.Height = Length.Fill;
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentTree.AppendRootComponent(ComponentRoot);
 
             ComponentTop = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
             ComponentTop.Name = "Top";
             ComponentTop.Width = Length.Fill;
-            ComponentTop.Height = new Length(20, UnitType.Unit);
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentTop.Height = new Length(50, UnitType.Pixel);
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentRoot.Components.Append(ComponentTop);
             ComponentTop.Parent = ComponentRoot;
 
@@ -57,7 +57,7 @@ namespace NextPlatform.Layout.Tests
             ComponentBottom.Name = "Bottom";
             ComponentBottom.Width = Length.Fill;
             ComponentBottom.Height = Length.Fill;
-            ComponentBottom.LayoutDirection = Controls.Abstractions.LayoutDirection.Horizontal;
+            ComponentBottom.LayoutDirection = LayoutDirection.Horizontal;
             ComponentRoot.Components.Append(ComponentBottom);
             ComponentBottom.Parent = ComponentRoot;
 
@@ -66,23 +66,23 @@ namespace NextPlatform.Layout.Tests
             ComponentLeft.Name = "Left";
             ComponentLeft.Width = new Length(1, UnitType.Ratio);
             ComponentLeft.Height = Length.Shrink;
-            ComponentLeft.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentLeft.LayoutDirection = LayoutDirection.Vertical;
             ComponentBottom.Components.Append(ComponentLeft);
             ComponentLeft.Parent = ComponentBottom;
 
             ComponentLeft1 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
             ComponentLeft1.Name = "Left1";
             ComponentLeft1.Width = Length.Fill;
-            ComponentLeft1.Height = new Length(10, UnitType.Unit);
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentLeft1.Height = new Length(40, UnitType.Pixel);
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentLeft.Components.Append(ComponentLeft1);
             ComponentLeft1.Parent = ComponentLeft;
 
             ComponentLeft2 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
             ComponentLeft2.Name = "Left2";
             ComponentLeft2.Width = Length.Fill;
-            ComponentLeft2.Height = new Length(20, UnitType.Unit);
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentLeft2.Height = new Length(40, UnitType.Pixel);
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentLeft.Components.Append(ComponentLeft2);
             ComponentLeft2.Parent = ComponentLeft;
 
@@ -91,23 +91,23 @@ namespace NextPlatform.Layout.Tests
             ComponentRight.Name = "Right";
             ComponentRight.Width = new Length(3, UnitType.Ratio);
             ComponentRight.Height = Length.Fill;
-            ComponentRight.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRight.LayoutDirection = LayoutDirection.Vertical;
             ComponentBottom.Components.Append(ComponentRight);
             ComponentRight.Parent = ComponentBottom;
 
             ComponentRight1 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
             ComponentRight1.Name = "Right1";
             ComponentRight1.Width = Length.Fill;
-            ComponentRight1.Height = new Length(35, UnitType.Unit);
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRight1.Height = new Length(40, UnitType.Pixel);
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentRight.Components.Append(ComponentRight1);
             ComponentRight1.Parent = ComponentRight;
 
             ComponentRight2 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
             ComponentRight2.Name = "Right2";
             ComponentRight2.Width = Length.Fill;
-            ComponentRight2.Height = new Length(20, UnitType.Unit);
-            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRight2.Height = new Length(80, UnitType.Pixel);
+            ComponentRoot.LayoutDirection = LayoutDirection.Vertical;
             ComponentRight.Components.Append(ComponentRight2);
             ComponentRight2.Parent = ComponentRight;
 
--- tests/NextPlatform.Layout.Tests/LayoutEngineTests.cs
+++ tests/NextPlatform.Layout.Tests/LayoutEngineTests.cs
@@ -37,8 +37,57 @@ namespace NextPlatform.Layout.Tests
             Assert.DoesNotContain(testData.ComponentLeft, componentTree.LeafComponents);
             Assert.DoesNotContain(testData.ComponentRight, componentTree.LeafComponents);
 
+            layoutEngine.ProcessLayout(new Size(800, 600, UnitType.Pixel), componentTree);
+        }
+
+        [Fact]
+        public void TestLayoutCalculation_Basic()
+        {
+            var layoutEngine = new LayoutEngine();
+            var componentTree = new ComponentTree();
+            var testData = new BasicTestData(componentTree);
 
             layoutEngine.ProcessLayout(new Size(800, 600, UnitType.Pixel), componentTree);
+
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentTop].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(800, layoutEngine.layoutData[testData.ComponentTop].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentTop].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentTop].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentBottom].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(800, layoutEngine.layoutData[testData.ComponentBottom].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(600, layoutEngine.layoutData[testData.ComponentBottom].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentBottom].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentLeft].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentLeft].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(130, layoutEngine.layoutData[testData.ComponentLeft].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentLeft].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentLeft1].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentLeft1].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(90, layoutEngine.layoutData[testData.ComponentLeft1].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentLeft1].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(90, layoutEngine.layoutData[testData.ComponentLeft2].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentLeft2].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(130, layoutEngine.layoutData[testData.ComponentLeft2].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(0, layoutEngine.layoutData[testData.ComponentLeft2].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentRight].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(800, layoutEngine.layoutData[testData.ComponentRight].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(600, layoutEngine.layoutData[testData.ComponentRight].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentRight].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(50, layoutEngine.layoutData[testData.ComponentRight1].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(800, layoutEngine.layoutData[testData.ComponentRight1].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(90, layoutEngine.layoutData[testData.ComponentRight1].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentRight1].AbsoluteBox.Left[UnitType.Pixel]);
+
+            Assert.Equal(90, layoutEngine.layoutData[testData.ComponentRight2].AbsoluteBox.Top[UnitType.Pixel]);
+            Assert.Equal(800, layoutEngine.layoutData[testData.ComponentRight2].AbsoluteBox.Right[UnitType.Pixel]);
+            Assert.Equal(170, layoutEngine.layoutData[testData.ComponentRight2].AbsoluteBox.Bottom[UnitType.Pixel]);
+            Assert.Equal(200, layoutEngine.layoutData[testData.ComponentRight2].AbsoluteBox.Left[UnitType.Pixel]);
         }
     }
 }
