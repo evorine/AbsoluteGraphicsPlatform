e418cc40570c0b9090d50b8a45c351033c5bbc41
Yilmaz Dogus <yilmaz@evorine.com>
2017-12-04 17:50:01 +0100

    Beautified test code

---
+++ tests/NextPlatform.Layout.Tests/BasicTestData.cs
@@ -0,0 +1,117 @@
+﻿using NextPlatform.Controls;
+using NextPlatform.Controls.Abstractions;
+using NextPlatform.Core;
+using NextPlatform.Metrics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextPlatform.Layout.Tests
+{
+    public class BasicTestData
+    {
+        /* Layout is:
+         * <component Width="Fill" Height="Fill" LayoutDirection="Vertical">
+         *   <component Width="Fill" Height="20u" />
+         *   <component Width="Fill" Height="Fill" LayoutDirection="Horizontal">
+         *     <component Width="1x" Height="Shrink" LayoutDirection="Vertical">
+         *       <component Width="Fill" Height="10u" />
+         *       <component Width="Fill" Height="20u" />
+         *     </component>
+         *     <component Width="3x" Height="Fill" LayoutDirection="Vertical">
+         *       <component Width="Fill" Height="35u" />
+         *       <component Width="Fill" Height="20u" />
+         *     </component>
+         *   </component>
+         * </component>
+         * */
+
+        public VisualElement ComponentRoot { get; }
+        public VisualElement ComponentTop { get; }
+        public VisualElement ComponentBottom { get; }
+        public VisualElement ComponentLeft { get; }
+        public VisualElement ComponentLeft1 { get; }
+        public VisualElement ComponentLeft2 { get; }
+        public VisualElement ComponentRight { get; }
+        public VisualElement ComponentRight1 { get; }
+        public VisualElement ComponentRight2 { get; }
+
+        public BasicTestData(ComponentTree ComponentTree)
+        {
+            ComponentRoot = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentRoot.Name = "Root";
+            ComponentRoot.Width = Length.Fill;
+            ComponentRoot.Height = Length.Fill;
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentTree.AppendRootComponent(ComponentRoot);
+
+            ComponentTop = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentTop.Name = "Top";
+            ComponentTop.Width = Length.Fill;
+            ComponentTop.Height = new Length(20, UnitType.Unit);
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRoot.Components.Append(ComponentTop);
+            ComponentTop.Parent = ComponentRoot;
+
+            ComponentBottom = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentBottom.Name = "Bottom";
+            ComponentBottom.Width = Length.Fill;
+            ComponentBottom.Height = Length.Fill;
+            ComponentBottom.LayoutDirection = Controls.Abstractions.LayoutDirection.Horizontal;
+            ComponentRoot.Components.Append(ComponentBottom);
+            ComponentBottom.Parent = ComponentRoot;
+
+            // LEFT
+            ComponentLeft = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentLeft.Name = "Left";
+            ComponentLeft.Width = new Length(1, UnitType.Ratio);
+            ComponentLeft.Height = Length.Shrink;
+            ComponentLeft.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentBottom.Components.Append(ComponentLeft);
+            ComponentLeft.Parent = ComponentBottom;
+
+            ComponentLeft1 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentLeft1.Name = "Left1";
+            ComponentLeft1.Width = Length.Fill;
+            ComponentLeft1.Height = new Length(10, UnitType.Unit);
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentLeft.Components.Append(ComponentLeft1);
+            ComponentLeft1.Parent = ComponentLeft;
+
+            ComponentLeft2 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentLeft2.Name = "Left2";
+            ComponentLeft2.Width = Length.Fill;
+            ComponentLeft2.Height = new Length(20, UnitType.Unit);
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentLeft.Components.Append(ComponentLeft2);
+            ComponentLeft2.Parent = ComponentLeft;
+
+            // RIGHT
+            ComponentRight = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentRight.Name = "Right";
+            ComponentRight.Width = new Length(3, UnitType.Ratio);
+            ComponentRight.Height = Length.Fill;
+            ComponentRight.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentBottom.Components.Append(ComponentRight);
+            ComponentRight.Parent = ComponentBottom;
+
+            ComponentRight1 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentRight1.Name = "Right1";
+            ComponentRight1.Width = Length.Fill;
+            ComponentRight1.Height = new Length(35, UnitType.Unit);
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRight.Components.Append(ComponentRight1);
+            ComponentRight1.Parent = ComponentRight;
+
+            ComponentRight2 = ComponentTree.ComponentFactory.CreateComponent<VisualElement>();
+            ComponentRight2.Name = "Right2";
+            ComponentRight2.Width = Length.Fill;
+            ComponentRight2.Height = new Length(20, UnitType.Unit);
+            ComponentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
+            ComponentRight.Components.Append(ComponentRight2);
+            ComponentRight2.Parent = ComponentRight;
+
+            ComponentTree.Recalculate();
+        }
+    }
+}
--- tests/NextPlatform.Layout.Tests/LayoutEngineTests.cs
+++ tests/NextPlatform.Layout.Tests/LayoutEngineTests.cs
@@ -12,119 +12,30 @@ namespace NextPlatform.Layout.Tests
         [Fact]
         public void TestBasic()
         {
-            /*
-             * <component Width="Fill" Height="Fill" LayoutDirection="Vertical">
-             *   <component Width="Fill" Height="20u" />
-             *   <component Width="Fill" Height="Fill" LayoutDirection="Horizontal">
-             *     <component Width="1x" Height="Shrink" LayoutDirection="Vertical">
-             *       <component Width="Fill" Height="10u" />
-             *       <component Width="Fill" Height="20u" />
-             *     </component>
-             *     <component Width="3x" Height="Fill" LayoutDirection="Vertical">
-             *       <component Width="Fill" Height="35u" />
-             *       <component Width="Fill" Height="20u" />
-             *     </component>
-             *   </component>
-             * </component>
-             * */
             var layoutEngine = new LayoutEngine();
             var componentTree = new ComponentTree();
+            var testData = new BasicTestData(componentTree);
 
-            var componentRoot = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentRoot.Name = "Root";
-            componentRoot.Width = Length.Fill;
-            componentRoot.Height = Length.Fill;
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentTree.AppendRootComponent(componentRoot);
+            Assert.Contains(testData.ComponentRoot, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentTop, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentBottom, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentLeft, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentLeft1, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentLeft2, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentRight, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentRight1, componentTree.AllComponents);
+            Assert.Contains(testData.ComponentRight2, componentTree.AllComponents);
 
-            var componentTop = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentTop.Name = "Top";
-            componentTop.Width = Length.Fill;
-            componentTop.Height = new Length(20, UnitType.Unit);
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentRoot.Components.Append(componentTop);
-            componentTop.Parent = componentRoot;
+            Assert.Contains(testData.ComponentTop, componentTree.LeafComponents);
+            Assert.Contains(testData.ComponentLeft1, componentTree.LeafComponents);
+            Assert.Contains(testData.ComponentLeft2, componentTree.LeafComponents);
+            Assert.Contains(testData.ComponentRight1, componentTree.LeafComponents);
+            Assert.Contains(testData.ComponentRight2, componentTree.LeafComponents);
 
-            var componentBottom = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentBottom.Name = "Bottom";
-            componentBottom.Width = Length.Fill;
-            componentBottom.Height = Length.Fill;
-            componentBottom.LayoutDirection = Controls.Abstractions.LayoutDirection.Horizontal;
-            componentRoot.Components.Append(componentBottom);
-            componentBottom.Parent = componentRoot;
-
-            // LEFT
-            var componentLeft = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentLeft.Name = "Left";
-            componentLeft.Width = new Length(1, UnitType.Ratio);
-            componentLeft.Height = Length.Shrink;
-            componentLeft.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentBottom.Components.Append(componentLeft);
-            componentLeft.Parent = componentBottom;
-
-            var componentLeft1 = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentLeft1.Name = "Left1";
-            componentLeft1.Width = Length.Fill;
-            componentLeft1.Height = new Length(10, UnitType.Unit);
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentLeft.Components.Append(componentLeft1);
-            componentLeft1.Parent = componentLeft;
-
-            var componentLeft2 = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentLeft2.Name = "Left2";
-            componentLeft2.Width = Length.Fill;
-            componentLeft2.Height = new Length(20, UnitType.Unit);
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentLeft.Components.Append(componentLeft2);
-            componentLeft2.Parent = componentLeft;
-
-            // RIGHT
-            var componentRight = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentRight.Name = "Right";
-            componentRight.Width = new Length(3, UnitType.Ratio);
-            componentRight.Height = Length.Fill;
-            componentRight.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentBottom.Components.Append(componentRight);
-            componentRight.Parent = componentBottom;
-
-            var componentRight1 = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentRight1.Name = "Right1";
-            componentRight1.Width = Length.Fill;
-            componentRight1.Height = new Length(35, UnitType.Unit);
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentRight.Components.Append(componentRight1);
-            componentRight1.Parent = componentRight;
-
-            var componentRight2 = componentTree.ComponentFactory.CreateComponent<VisualElement>();
-            componentRight2.Name = "Right2";
-            componentRight2.Width = Length.Fill;
-            componentRight2.Height = new Length(20, UnitType.Unit);
-            componentRoot.LayoutDirection = Controls.Abstractions.LayoutDirection.Vertical;
-            componentRight.Components.Append(componentRight2);
-            componentRight2.Parent = componentRight;
-
-            componentTree.Recalculate();
-
-            Assert.Contains(componentRoot, componentTree.AllComponents);
-            Assert.Contains(componentTop, componentTree.AllComponents);
-            Assert.Contains(componentBottom, componentTree.AllComponents);
-            Assert.Contains(componentLeft, componentTree.AllComponents);
-            Assert.Contains(componentLeft1, componentTree.AllComponents);
-            Assert.Contains(componentLeft2, componentTree.AllComponents);
-            Assert.Contains(componentRight, componentTree.AllComponents);
-            Assert.Contains(componentRight1, componentTree.AllComponents);
-            Assert.Contains(componentRight2, componentTree.AllComponents);
-
-            Assert.Contains(componentTop, componentTree.LeafComponents);
-            Assert.Contains(componentLeft1, componentTree.LeafComponents);
-            Assert.Contains(componentLeft2, componentTree.LeafComponents);
-            Assert.Contains(componentRight1, componentTree.LeafComponents);
-            Assert.Contains(componentRight2, componentTree.LeafComponents);
-
-            Assert.DoesNotContain(componentRoot, componentTree.LeafComponents);
-            Assert.DoesNotContain(componentBottom, componentTree.LeafComponents);
-            Assert.DoesNotContain(componentLeft, componentTree.LeafComponents);
-            Assert.DoesNotContain(componentRight, componentTree.LeafComponents);
+            Assert.DoesNotContain(testData.ComponentRoot, componentTree.LeafComponents);
+            Assert.DoesNotContain(testData.ComponentBottom, componentTree.LeafComponents);
+            Assert.DoesNotContain(testData.ComponentLeft, componentTree.LeafComponents);
+            Assert.DoesNotContain(testData.ComponentRight, componentTree.LeafComponents);
 
 
             layoutEngine.ProcessLayout(new Size(800, 600, UnitType.Pixel), componentTree);
