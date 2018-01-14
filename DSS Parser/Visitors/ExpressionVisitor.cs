2478f12371866f0eacb4d7d68d96c52a83782502
Yilmaz Dogus <yilmaz@evorine.com>
2018-01-14 01:13:06 +0100

    DSS Parser

---
+++ DSS Parser/Visitors/ExpressionVisitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+
+namespace AbsoluteGraphicsPlatform.DSS.Visitors
+{
+    public class ExpressionVisitor : DSSParserBaseVisitor<Expression>
+    {
+        public override Expression VisitExpression([NotNull] DSSParser.ExpressionContext context)
+        {
+
+            return base.VisitExpression(context);
+        }
+    }
+}
