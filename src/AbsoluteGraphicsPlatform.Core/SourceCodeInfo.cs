33218b70549528e477748ff83207bec85c7915d5
Yilmaz Dogus <yilmaz@evorine.com>
2018-01-16 19:16:03 +0100

    +SourceCodeInfo class

---
+++ src/AbsoluteGraphicsPlatform.Core/SourceCodeInfo.cs
@@ -0,0 +1,44 @@
+﻿// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AbsoluteGraphicsPlatform
+{
+    public class SourceCodeInfo
+    {
+        readonly string sourceName;
+        readonly Func<Stream> streamGetter;
+
+
+        public SourceCodeInfo(FileInfo fileInfo)
+        {
+            if (fileInfo == null) throw new ArgumentNullException(nameof(fileInfo));
+
+            sourceName = fileInfo.Name;
+            streamGetter = () => fileInfo.OpenRead();
+        }
+
+        public SourceCodeInfo(string sourceName, Stream stream)
+        {
+            if (sourceName == null) throw new ArgumentNullException(nameof(sourceName));
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+
+            this.sourceName = sourceName;
+            streamGetter = () => stream;
+        }
+
+        /// <summary>
+        /// Gets the name of the source.
+        /// </summary>
+        public string SourceName => sourceName;
+
+        /// <summary>
+        /// Gets the source stream.
+        /// </summary>
+        public Stream GetStream() => streamGetter();
+    }
+}
