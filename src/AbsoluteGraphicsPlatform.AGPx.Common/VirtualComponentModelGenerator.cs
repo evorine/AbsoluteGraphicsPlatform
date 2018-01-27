1d83fd759a37d5b8b4d700e21562201dc9893074
Yilmaz Dogus <yilmaz@evorine.com>
2018-01-27 17:33:13 +0100

    Started working on Virtual Component Model

---
+++ src/AbsoluteGraphicsPlatform.AGPx.Common/VirtualComponentModelGenerator.cs
@@ -0,0 +1,25 @@
+﻿// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using AbsoluteGraphicsPlatform.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbsoluteGraphicsPlatform.AGPx
+{
+    public class VirtualComponentModelGenerator
+    {
+        public VirtualComponentModelGenerator(ComponentTemplate[] componentTemplates)
+        {
+            AllComponentTemplates = componentTemplates;
+        }
+
+        public ComponentTemplate[] AllComponentTemplates { get; }
+
+        public IVirtualComponentModel ProcessTemplate(ComponentTemplate template)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/AbsoluteGraphicsPlatform.Abstractions/ComponentModel/IVirtualComponentModel.cs
@@ -0,0 +1,14 @@
+﻿// Licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using AbsoluteGraphicsPlatform.Abstractions.Components;
+using System;
+using System.Collections.Generic;
+
+namespace AbsoluteGraphicsPlatform.ComponentModel
+{
+    public class IVirtualComponentModel
+    {
+        IComponent RootComponent { get; set; }
+    }
+}
